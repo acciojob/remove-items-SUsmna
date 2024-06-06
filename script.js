8feb0a232a6b9d284d292008e36e110f24232ef3
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-06-06 17:50:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeSelectedColor(){
+	const colorSelect = document.getElementById("colorSelect");
+	const selectedIndex = colorSelect.selectedIndex;
+	if (selectedIndex !== -1){
+		colorSelect.remove(selectedIndex);
+	}
+}
